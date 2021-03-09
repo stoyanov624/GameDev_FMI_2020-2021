23ac1a28a4639776128e013b44ee51a93f0a98dc
Ivailo Stoyanov <paradoxa624@gmail.com>
2021-03-09 02:35:06 +0200

    made player movement and walk animation

---
+++ GameDev1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    [SerializeField]
+    private float speed;
+    private Rigidbody rb;
+    private Vector3 input;
+
+
+    void Start() {
+       rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update() {
+        input = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+    }
+    
+    void FixedUpdate() {
+        rb.AddRelativeForce(speed*input);
+    }
+}
---
+++ Homework1/AlienPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        direction = new Vector2(Input.GetAxis("Horizontal"),0);
+    }
+
+    void FixedUpdate() {
+        MoveCharacter(direction);
+    }
+
+    void MoveCharacter(Vector2 direction) {
+        rb.MovePosition((Vector2)transform.position + (direction * movementSpeed * Time.deltaTime));
+        animator.SetFloat("horizontal",Mathf.Abs(direction.x));
+        
+        if((direction.x > 0 && !facingRight) || (direction.x < 0 && facingRight))
+            Flip();
+    }
+
+    void Flip() {
+       facingRight = !facingRight;
+       transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+}
