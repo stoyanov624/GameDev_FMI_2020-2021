07694e64761e9b382e3c98d7bc55d49f2077705e
Ivailo Stoyanov <paradoxa624@gmail.com>
2021-03-11 15:31:08 +0200

    resetted everything right

---
+++ Homework1/AlienPlatformer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    
+    [SerializeField]
+    private float movingSpeed;
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+
+    void Start() {
+        startPosition = transform.position;
+        endPosition = new Vector2(transform.position.x + 3,transform.position.y);
+    }
+    
+    void Update() {
+        transform.position = Vector2.Lerp(startPosition,endPosition, Mathf.PingPong(Time.time * movingSpeed,1.0f));        
+    }
+}
